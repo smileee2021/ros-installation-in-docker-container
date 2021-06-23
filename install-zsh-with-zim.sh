ab0a37e6b921f6628b484635968675401b868ba6
smileee2021 <85753143+smileee2021@users.noreply.github.com>
2021-06-23 22:43:53 +0800

    [update] Add border

--- install-zsh-with-zim.sh
+++ install-zsh-with-zim.sh
@@ -10,5 +10,5 @@ sed -i 's/asciiship/eriner/g' ~/.zimrc
 sed -i 's/set -e//g' /ros_entrypoint.sh
 sed -i 's/setup.bash/setup.sh/g' /ros_entrypoint.sh
 
-echo 'Please type the following instruction:\n\n\e[0;36mzimfw install\nsource /ros_entrypoint.sh\e[0m'
+echo '┌────────────────────────────────────────┐\n│                                        │\n│ Please type the following instruction: │\n│                                        │\n│\e[0;36m zimfw install\e[0m                          │\n│ \e[0;36msource /ros_entrypoint.sh\e[0m              │\n│ \e[0;36mzsh\e[0m                                    │\n│                                        │\n└────────────────────────────────────────┘'
 zsh
