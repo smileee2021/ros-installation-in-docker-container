eb2087db81e63ca5a8fcf3550a439b968d9a1ec3
smileee2021 <85753143+smileee2021@users.noreply.github.com>
2021-06-23 17:38:39 +0800

    Create install-zsh-with-zim.sh

---
+++ install-zsh-with-zim.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# install necessary package
+apt update
+apt install curl zsh
+
+# install zsh with zim
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
+
+# exit
+echo 'Please exec container terminal with zsh again, and then type the following instruction:\n\n\e[0;36msource /opt/ros/foxy/setup.bash\nsource ros_entrypoint.sh\e[0m'
+exit
