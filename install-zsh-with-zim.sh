b421695316719bca5fd86d8eede82bffd3b9f3fa
smileee2021 <85753143+smileee2021@users.noreply.github.com>
2021-06-23 22:09:02 +0800

    [fix] Modify extension name

--- install-zsh-with-zim.sh
+++ install-zsh-with-zim.sh
@@ -8,6 +8,7 @@ apt install curl zsh
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 sed -i 's/asciiship/eriner/g' ~/.zimrc
 sed -i 's/set -e//g' /ros_entrypoint.sh
+sed -i 's/setup.bash/setup.sh/g' /ros_entrypoint.sh
 
 echo 'Please type the following instruction:\n\n\e[0;36mzimfw install\nsource /ros_entrypoint.sh\e[0m'
 zsh
