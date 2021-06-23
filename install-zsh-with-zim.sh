2a026a62d54185fc90dc8a3f44a7a8a8152eb2d9
smileee2021 <85753143+smileee2021@users.noreply.github.com>
2021-06-23 18:01:59 +0800

    [fix] Update zim theme

--- install-zsh-with-zim.sh
+++ install-zsh-with-zim.sh
@@ -6,7 +6,8 @@ apt install curl zsh
 
 # install zsh with zim
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
+sed -i 's/asciiship/eriner/g' ~/.zimrc
+zimfw install
 
 # exit
 echo 'Please exec container terminal with zsh again, and then type the following instruction:\n\n\e[0;36msource /opt/ros/foxy/setup.bash\nsource ros_entrypoint.sh\e[0m'
