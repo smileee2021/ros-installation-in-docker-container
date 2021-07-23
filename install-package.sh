8adeed2115e90695d9a8a36b7bb9e707765c7cf5
smileee2021 <85753143+smileee2021@users.noreply.github.com>
2021-07-23 11:55:22 +0800

    Add powerlevel10k module

--- install-package.sh
+++ install-package.sh
@@ -10,5 +10,7 @@ sed -i 's/asciiship/eriner/g' ~/.zimrc
 sed -i 's/set -e//g' /ros_entrypoint.sh
 sed -i 's/setup.bash/setup.sh/g' /ros_entrypoint.sh
 
+echo "zmodule romkatv/powerlevel10k" >> ~/.zimrc
+
 echo '┌────────────────────────────────────────┐\n│                                        │\n│ Please type the following instruction: │\n│                                        │\n│\e[0;36m zimfw install\e[0m                          │\n│ \e[0;36msource /ros_entrypoint.sh\e[0m              │\n│ \e[0;36mzsh\e[0m                                    │\n│                                        │\n└────────────────────────────────────────┘'
 zsh
